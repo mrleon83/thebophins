3f1fcf519db9556ba56c271f0081d1f37ce21fa2
Leon Kimpton <leon010983@yahoo.co.uk>
2017-10-30 17:18:24 +0000

    Add files via upload
    Added Spotify API to the media section

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -2,15 +2,15 @@
   <br><br>
   <br><br>
   <div class="row">
-
+<!--
   	<div class="col md-8">
   		<div class="fb-like" data-href="https://www.facebook.com/thebophins/" data-width="100" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div></br>
-	<script src="https://apis.google.com/js/platform.js"></script>
+	     <script src="https://apis.google.com/js/platform.js"></script>
 	<div class="g-ytsubscribe" data-channel="thebophins" data-layout="default" data-count="hidden"></div>
   </div>
-
-  <Div class="col md-4">
-  <span style="color:#666"><p>Site created by <a href="http://www.leonkimpton.org" data-toggle="tooltip">www.leonkimpton.org</a></p></span>
+-->
+  <Div class="col md-12">
+  <span style="color:#666"><p>Site created by <a href="http://www.leonkimpton.org" data-toggle="tooltip" style="color:#666">Dragon Heart Studio</a></p></span>
 	</Div>
 </div>
 </footer>
--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -1,4 +1,51 @@
 @extends('header')
 @section('title' , ' - Media')
 @section('content')
+
+<style>
+.notmobile{
+	width: 100%;
+}
+
+</style>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<iframe src="https://open.spotify.com/embed?uri=spotify:album:1zdtAIy3TqImZJr73N8Dvd" width="300" height="400" frameborder="0" allowtransparency="true">
+			</iframe>
+		</div>
+		<div class="col-md-4">
+				<img src="images/newswag.jpg" class="notmobile">
+		</div>
+		<div class="col-md-4">
+			<p style="color:#fff; font-family: comic-sans;">blurb about the album etc Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+</br>
+<hr style="color: #fff">
+</br>
+	<div class="row">
+		<div class="col-md-4">
+		<iframe src="https://open.spotify.com/embed?uri=spotify:album:1w2WVjAokLf9Ju2dbxETg2" width="300" height="400" frameborder="0" allowtransparency="true">	
+		</iframe>
+		</div>
+		<div class="col-md-4">
+			<img src="images/xrayaction.jpg" class="notmobile">
+		</div>
+		<div class="col-md-4">
+			<p style="color:#fff;font-family: comic-sans;">blurb about the album etc Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
 @endsection
+
+
+
+
